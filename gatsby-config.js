c99f784e484bb709266c0afe31354ed04c514bee
dougajmcdonald <dougajmcdonald@gmail.com>
2017-07-31 21:13:26 +0100

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata: {
+    title: `Doug McDonald`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [] // just in case those previously mentioned remark plugins sound cool :)
+      }
+    }
+  ]
+}
---
+++ src/pages/2017-07-31-demo-post/index.md
@@ -0,0 +1,7 @@
+---
+path: "/demo-post"
+date: "2017-07-31T17:12:33.962Z"
+title: "Super awesome markdown gatsby post"
+---
+
+Oooooh-weeee, my first blog post!
